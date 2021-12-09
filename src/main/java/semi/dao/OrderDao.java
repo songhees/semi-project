042bc80101b01525ce6272630a8de8fd59d6ebfe
songhees <osh4880@gmail.com>
2021-12-09 12:54:26 +0900

    [UPDATE] 배송지 계산 코드 추가

--- src/main/java/semi/dao/OrderDao.java
+++ src/main/java/semi/dao/OrderDao.java
@@ -147,7 +147,7 @@ public class OrderDao {
 		String sql = "select i.order_no, "
 				+ "     i.order_product_price, i.order_product_quantity, "
 				+ "		t.product_item_no, t.product_color, t.product_size, "
-				+ "		p.product_no, p.product_name, "
+				+ "		p.product_no, p.product_name, p.product_price, "
 				+ "		s.thumbnail_image_url "
 				+ "from semi_order_item i, semi_product_item t, semi_product p, semi_product_thumbnail_image s "
 				+ "where i.order_no = ? "
@@ -172,6 +172,7 @@ public class OrderDao {
 			item.setProductNo(rs.getInt("product_no"));
 			item.setProductName(rs.getString("product_name"));
 			item.setThumbnailUrl(rs.getString("thumbnail_image_url"));
+			item.setProductPrice(rs.getLong("product_price"));
 			itemsInfo.add(item);
 		}
 		rs.close();
